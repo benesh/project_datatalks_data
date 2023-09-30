ec07c71c954c69a3d6dd77dc7b78031be9542999
Ben_Omar <bennessh@gmail.com>
2023-09-30 23:35:54 +0200

    mise ade l'evolution spark et docker

---
+++ flows_city_bike.py
@@ -0,0 +1,31 @@
+from get_data_services.flow_task_get_data_services import File_properties , run_steps_fetching
+from prefect import flow
+
+@flow(name="LANCHING NY BIKE FILES FETCHING ")
+def url_creating_citi_bike(years_list,interval_month):
+    print(years_list,interval_month)
+    start_month= interval_month[0]
+    end_month_include= interval_month[1]
+    file_props = []
+    for year in years_list:
+        while end_month_include >= start_month:
+            #https://s3.amazonaws.com/tripdata/201309-citibike-tripdata.zip
+            if year < 2017 :
+                if start_month not in (6,7) and year != 2022:
+                    url_file = f'https://s3.amazonaws.com/tripdata/{year}{start_month:02}-citbike-tripdata.zip'
+                else :
+                    url_file = f'https://s3.amazonaws.com/tripdata/{year}{start_month:02}-citibike-tripdata.zip'
+            else:
+                url_file = f'https://s3.amazonaws.com/tripdata/{year}{start_month:02}-citibike-tripdata.csv.zip'
+            file_name = f'{year}{start_month:02}-citibike-tripdata'
+            file_extension= 'csv'
+            file_zip_suffix = 'zip'
+            file_props.append( File_properties(url_file,file_name,file_extension,file_zip_suffix,year,start_month) )
+            start_month += 1
+
+    run_steps_fetching(file_props)
+
+if __name__=='__main__':
+    year = [2023]
+    month = (1,6)
+    url_creating_citi_bike(year, month)
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from city_bike_url_creating_services import city_bike_url_creating
+from prefect import flow
+from flows_city_bike import url_creating_citi_bike
 
-url_file ='https://s3.amazonaws.com/tripdata/201309-citibike-tripdata.zip'
-directory_file='/home/benomar/datawarehouse/zip_data/2013/01/201309-citibike-tripdata.zip'
+@flow()
+def start_flow_in_main():
+    year = [2023]
+    month = (4, 4)
+    url_creating_citi_bike(year, month)
+
+
 
 if __name__=='__main__':
-    year = [2023]
-    month = (5,5)
-    city_bike_url_creating.url_creating_citi_bike(year,month)
+
+    start_flow_in_main()
 
